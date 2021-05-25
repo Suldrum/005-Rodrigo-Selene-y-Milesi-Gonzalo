febc75db341fe7eec6df36e590a0118d88d58754
Suldrum <selenerodrigo@outlook.com>
2021-05-25 14:26:08 -0300

    agregado UserView.php

---
+++ views/UserView.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once('View.php');
+
+class UserView extends View {
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function showLogin($error=null) {
+        $this->getSmarty()->assign('title', "Login");
+        $this->getSmarty()->assign('error', $error);        
+        $this->getSmarty()->assign('home', BASE_URL.'home');
+        $this->getSmarty()->display('templates/login.tpl');
+    }
+
+    public function showRegistrar($error=null) {
+        $this->getSmarty()->assign('title', "Login");
+        $this->getSmarty()->assign('error', $error);        
+        $this->getSmarty()->assign('home', BASE_URL.'tasks');
+        $this->getSmarty()->display('templates/registro.tpl');
+    }    
+}
