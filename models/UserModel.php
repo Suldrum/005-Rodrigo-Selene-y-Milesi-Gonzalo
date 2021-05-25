bd0fc019c313fb27e7096041b1981a447093347c
Suldrum <selenerodrigo@outlook.com>
2021-05-25 14:11:48 -0300

    agregado UserModel.php

---
+++ models/UserModel.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once('Model.php');
+
+class UserModel extends Model {
+
+    public function getUserByUsername($username) {
+        $query = $this->getDb()->prepare('SELECT * FROM `entrenador` WHERE entrenador = ?');
+        $query->execute(array(($username)));
+        return $query->fetch(PDO::FETCH_OBJ);
+    }
+
+    public function add($name, $lastName, $username, $email, $pass) {
+
+        $passEnc = password_hash($pass, PASSWORD_DEFAULT);
+
+        $query = $this->getDb()->prepare('INSERT INTO `entrenador` (nombre, apellido, apodo, email, contraseña) 
+                                            VALUES (?, ?, ?, ?, ?)');
+
+        $query->execute([$name, $lastName, $username, $email, $passEnc]);
+
+    }
+}
